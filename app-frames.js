25f565372a97eb2bc3c4211e45016bded2243adc
spritesarebetter <synthegrate@yahoo.com>
2026-09-09 23:05:43 +0200

    Show Color 0 correctly in frame thumbnails

--- app-frames.js
+++ app-frames.js
@@ -14,10 +14,10 @@
     frame.sprites.forEach((s,i)=>{
       if(!s.visible)return;const bx=ox(s,i)-b.minX,by=oy(s,i)-b.minY;
       for(let y=0;y<n;y++){
-        const a=s.lines[y];if(s.transparent&&a.color===0)continue;
+        const a=s.lines[y];
         for(let x=0;x<n;x++){
-          if(!s.mask[y][x])continue;const px=bx+x,py=by+y;if(px<0||py<0||px>=b.w||py>=b.h)continue;
-          const p=py*b.w+px,cur=cells[p];if(i>0&&a.or){if(cur>=0)cells[p]=(cur|a.color)&15}else if(cur<0)cells[p]=a.color;
+          const col=s.mask[y][x]?a.color:0;if(s.transparent&&col===0)continue;const px=bx+x,py=by+y;if(px<0||py<0||px>=b.w||py>=b.h)continue;
+          const p=py*b.w+px,cur=cells[p];if(i>0&&a.or&&cur>=0)cells[p]=(cur|col)&15;else if(cur<0)cells[p]=col;
         }
       }
     });

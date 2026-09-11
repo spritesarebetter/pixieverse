d61c83e3daeea5a7bd7601c658ec9303c263daec
spritesarebetter <synthegrate@yahoo.com>
2026-09-11 15:59:08 +0200

    Add Pixieverse desktop host

---
+++ electron-main.js
@@ -0,0 +1,27 @@
+'use strict';
+const {app,BrowserWindow,shell}=require('electron');
+const path=require('path');
+
+function createWindow(){
+  const win=new BrowserWindow({
+    width:1600,
+    height:950,
+    minWidth:1060,
+    minHeight:650,
+    backgroundColor:'#101216',
+    autoHideMenuBar:true,
+    webPreferences:{
+      contextIsolation:true,
+      nodeIntegration:false,
+      sandbox:true
+    }
+  });
+  win.loadFile(path.join(__dirname,'index.html'));
+  win.webContents.setWindowOpenHandler(({url})=>{shell.openExternal(url);return{action:'deny'}});
+}
+
+app.whenReady().then(()=>{
+  createWindow();
+  app.on('activate',()=>{if(BrowserWindow.getAllWindows().length===0)createWindow()});
+});
+app.on('window-all-closed',()=>{if(process.platform!=='darwin')app.quit()});
